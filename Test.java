0fe5647e7bb7fa91ba678540a469a07c4c1b8b7e
RainBamboo <46697655+rbamb@users.noreply.github.com>
2023-04-20 21:54:33 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,12 +1,15 @@
-if (optString.equals("callSync")) {
-  if (d.f24142e == null) {
-      d.f24142e = context.getApplicationContext();
-  }
-  if (optJSONArray != null && optJSONArray.length() == 2) {
-      method = FH.class.getMethod("callSync", Integer.TYPE, String.class);
-  } else if (optJSONArray == null || optJSONArray.length() >= 2) {
-      method = FH.class.getMethod("callSync", Integer.TYPE, String.class, Class[].class, Object[].class);
-  } else {
-      return new Pair<>(-6, "");
+for (int i2 = 0; i2 < a.size(); i2++) {
+  ApkInfo apkInfo = a.get(i2);
+  if (apkInfo.intentFilters != null) {
+    for (int i3 = 0; i3 < apkInfo.intentFilters.size(); i3++) {
+      lVar = apkInfo.intentFilters.get(i3);
+      if (lVar.f24167d.match(this.a.getAction(), 
+                             this.a.getType(), 
+                             this.a.getScheme(), 
+                             this.a.getData(), 
+                             this.a.getCategories(), "PIF") >= 0) {
+        Class<?> loadClass = apkInfo.classLoader.loadClass(lVar.b);
+      }
+    }
   }
 }
