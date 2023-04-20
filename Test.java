5f5f9b028c0aebb41a07680c045c787e5319e77d
RainBamboo <46697655+rbamb@users.noreply.github.com>
2023-04-20 21:30:08 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,14 +1,12 @@
-public class MainActivity extends Activity {
-  static {
-    System.loadLibrary("demo");
+if (optString.equals("callSync")) {
+  if (d.f24142e == null) {
+      d.f24142e = context.getApplicationContext();
   }
-  public native String nativeGetString();
-  
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    String str = nativeGetString();
-    Log.d("String from native", str);
+  if (optJSONArray != null && optJSONArray.length() == 2) {
+      method = FH.class.getMethod("callSync", Integer.TYPE, String.class);
+  } else if (optJSONArray == null || optJSONArray.length() >= 2) {
+      method = FH.class.getMethod("callSync", Integer.TYPE, String.class, Class[].class, Object[].class);
+  } else {
+      return new Pair<>(-6, "");
   }
 }
-
