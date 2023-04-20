3941fff394d9830008e306116c9a45a4ae9e3848
RainBamboo <46697655+rbamb@users.noreply.github.com>
2023-04-20 21:55:06 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,11 @@
-for (int i2 = 0; i2 < a.size(); i2++) {
-  ApkInfo apkInfo = a.get(i2);
-  if (apkInfo.intentFilters != null) {
-    for (int i3 = 0; i3 < apkInfo.intentFilters.size(); i3++) {
-      lVar = apkInfo.intentFilters.get(i3);
-      if (lVar.f24167d.match(this.a.getAction(), 
-                             this.a.getType(), 
-                             this.a.getScheme(), 
-                             this.a.getData(), 
-                             this.a.getCategories(), "PIF") >= 0) {
-        Class<?> loadClass = apkInfo.classLoader.loadClass(lVar.b);
-      }
-    }
+for (int i3 = 0; i3 < apkInfo.intentFilters.size(); i3++) {
+  lVar = apkInfo.intentFilters.get(i3);
+  if (lVar.f24167d.match(this.a.getAction(), 
+                         this.a.getType(), 
+                         this.a.getScheme(), 
+                         this.a.getData(), 
+                         this.a.getCategories(), "PIF") >= 0) {
+    Class<?> loadClass = apkInfo.classLoader.loadClass(lVar.b);
   }
 }
+
