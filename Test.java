caa4cbc11e724d7f75b2a8b4b9d7564f358d0baa
RainBamboo <46697655+rbamb@users.noreply.github.com>
2023-04-23 01:05:41 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,13 +3,11 @@ public static Pair invokeMethod(Context context, String str) {
   String optString = jSONObject.optString("f");
   JSONArray optJSONArray = jSONObject.optJSONArray("p");
   if (optString.equals("callSync")) {
-    if (com.baidu.sofire.b.b.f52834e == null) {
-        com.baidu.sofire.b.b.f52834e = context.getApplicationContext();
-    }
     if (optJSONArray != null && optJSONArray.length() == 2) {
         method = FH.class.getMethod("callSync", Integer.TYPE, String.class);
     } else if (optJSONArray == null || optJSONArray.length() >= 2) {
-        method = FH.class.getMethod("callSync", Integer.TYPE, String.class, Class[].class, Object[].class);
+        method = FH.class.getMethod("callSync", Integer.TYPE, String.class, 
+                                    Class[].class, Object[].class);
     } else {
         return new Pair<>(-6, "");
     }
