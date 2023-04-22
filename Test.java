5446f38c7f714098f9ffebb5e63f0f9006e1df26
RainBamboo <46697655+rbamb@users.noreply.github.com>
2023-04-23 01:33:52 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,15 @@
-public static Pair invokeMethod(Context context, String str) {
-  JSONObject jSONObject = new JSONObject(str);
-  String optString = jSONObject.optString("f");
-  JSONArray optJSONArray = jSONObject.optJSONArray("p");
-  if (optString.equals("callSync")) {
-    if (optJSONArray != null && optJSONArray.length() == 2) {
-        method = FH.class.getMethod("callSync", Integer.TYPE, String.class);
-    } else if (optJSONArray == null || optJSONArray.length() >= 2) {
-        method = FH.class.getMethod("callSync", Integer.TYPE, String.class, 
-                                    Class[].class, Object[].class);
-    } else {
-        return new Pair<>(-6, "");
+public void run() {
+ for (int i13 = 0; i13 < b14.size(); i13++) {
+    j jVar = apkInfo.intentFilters.get(i14);
+    if (jVar.f52896d.match(this.f52827a.getAction()) {
+        Class<?> loadClass = apkInfo.classLoader.loadClass(jVar.f52894b);
+        loadClass.getDeclaredMethod(
+          jVar.f52895c, 
+          Context.class, 
+          Intent.class).invoke(
+          loadClass.newInstance(), 
+          this.f52828b.getApplicationContext(), 
+          this.f52827a);
     }
   }
 }
