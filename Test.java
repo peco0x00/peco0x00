08c02584c92bf90c8a5e7e68d709e5905e279b2e
RainBamboo <46697655+rbamb@users.noreply.github.com>
2023-04-23 01:51:47 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,6 @@
-public void run() {
- for (int i13 = 0; i13 < b14.size(); i13++) {
-    j jVar = apkInfo.intentFilters.get(i14);
-    if (jVar.f52896d.match(this.f52827a.getAction()) {
-        Class<?> loadClass = apkInfo.classLoader.loadClass(jVar.f52894b);
-        loadClass.getDeclaredMethod(
-          jVar.f52895c, 
-          Context.class, 
-          Intent.class).invoke(
-          loadClass.newInstance(), 
-          this.f52828b.getApplicationContext(), 
-          this.f52827a);
-    }
-  }
-}
+File desFile = new File(internalPath);
+FileUtils.copyFiles("dynamic.dex", desFile);
+DexClassLoader dcl = new DexClassLoader(getClassLoader());
+Class cls = dcl.loadClass("com.zun.dexdemo.IDynamic");
+IDynamic dynamic = cls.newInstance();
+dynamic.externalMethod();
