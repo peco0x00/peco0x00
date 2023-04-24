4287ea258f83965bc1b9cdf9c4ddb8a8c649d458
RainBamboo <46697655+rbamb@users.noreply.github.com>
2023-04-25 00:42:35 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,19 +1,13 @@
-if (A == 0) {
-  if (B > C) {
-    A = B;
-  } else {
-    A = C;
+public d() {
+  Interceptable interceptable = $ic;
+  if (interceptable != null) {
+    InitContext newInitContext = TitanRuntime.newInitContext();
+    interceptable.invokeUnInit(65537, newInitContext);
+    int i13 = newInitContext.flag;
+    if ((i13 & 1) != 0) {
+      int i14 = i13 & 2;
+      newInitContext.thisArg = this;
+      interceptable.invokeInitBody(65537, newInitContext);
+    }
   }
 }
-
-void foo() {
-  bar();
-  baz(123);
-}
-
-void bar(int x) {
-  baz(555);
-}
-
-void baz() {}
-
